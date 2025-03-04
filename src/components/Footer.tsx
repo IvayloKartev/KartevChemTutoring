4529daf6966e773f4afe186438bc29269eff6367
IvayloKartev <ivaylokartev@gmail.com>
2025-03-04 10:18:57 -0500

    Fixed links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Footer.scss'
+import RedditIcon from '@mui/icon-material/Reddit';
+import DiscordIcon from '@mui/icon-material/Discord';
 
 function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/IvayloKartev/KartevChemTutoring" target="_blank" rel="noreferrer"><RedditIcon/></a>
+        <a href="https://www.discord.com" target="_blank" rel="noreferrer"><DiscordIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Ivaylo Kartev</a> with 💜</p>
+      <p>Build by <a href="https://github.com/IvayloKartev/KartevChemTutoring" target="_blank" rel="noreferrer">Ivaylo Kartev</a></p>
     </footer>
   );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,8 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
 import avatar from '../assets/images/portret-circle.png';
-
+import RedditIcon from '@mui/icons-material/Reddit';
+import DiscordIcon from '@mui/icons-material/Discord';
 function Main() {
 
   return (
@@ -14,15 +15,15 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/ivaylokartv" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.reddit.com/user/IvayloKartev/" target="_blank" rel="noreferrer"><RedditIcon/></a>
+            <a href="https://www.discord.com" target="_blank" rel="noreferrer"><DiscordIcon/></a>
           </div>
           <h1>Ivaylo Kartev</h1>
           <p>Chemistry Tutor</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/ivaylokartev" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.reddit.com/user/IvayloKartev/" target="_blank" rel="noreferrer"><RedditIcon/></a>
+            <a href="https://www.discord.com" target="_blank" rel="noreferrer"><DiscordIcon/></a>
           </div>
         </div>
       </div>
